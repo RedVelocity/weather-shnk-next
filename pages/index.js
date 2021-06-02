81b1710a4f9914b877cae7da6d009b438e031218
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-06-02 11:08:56 +0530

    added head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Head from 'next/head';
+
 import WeatherChart from '../components/WeatherChart';
 import WeatherCard from '../components/WeatherCard';
 import WeatherMap from '../components/WeatherMap';
@@ -6,18 +8,28 @@ import SearchCard from '../components/SearchCard';
 
 const Home = () => {
   return (
-    <div className="flex-1 w-full max-w-screen-lg mx-auto">
-      <div className="grid max-w-xl gap-4 px-4 mx-auto md:max-w-full md:grid-cols-3">
-        <SearchCard />
-        <WeatherCard />
-        <div className="md:col-start-2 md:row-span-2 md:col-span-2 md:row-start-1">
-          <WeatherChart />
-        </div>
-        <div className="md:col-span-full">
-          <WeatherMap />
+    <>
+      <Head>
+        <title>SHNK | Weather</title>
+        <link rel="icon" type="image/png" href="/weather-icon.png" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Weather lookup from SHNK" />
+        <meta name="keywords" content="weather, javascript, html, css" />
+        <meta name="author" content="shnk.tech" />
+      </Head>
+      <div className="flex-1 w-full max-w-screen-lg mx-auto">
+        <div className="grid max-w-xl gap-4 px-4 mx-auto md:max-w-full md:grid-cols-3">
+          <SearchCard />
+          <WeatherCard />
+          <div className="md:col-start-2 md:row-span-2 md:col-span-2 md:row-start-1">
+            <WeatherChart />
+          </div>
+          <div className="md:col-span-full">
+            <WeatherMap />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
