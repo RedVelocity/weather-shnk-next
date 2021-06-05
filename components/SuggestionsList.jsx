28f1a78b5a4efa6c6a299526a9375fce47180209
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-06-05 11:02:09 +0530

    reverted to list render based on css

--- components/SuggestionsList.jsx
+++ components/SuggestionsList.jsx
@@ -17,37 +17,37 @@ const SuggestionsList = ({
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClick, false);
-    return () => document.removeEventListener('mousedown', handleClick, false);
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
   }, []);
 
   return (
-    showSuggestions && (
-      <div
-        ref={listNode}
-        className={`absolute bg-white w-full rounded-xl shadow p-2 z-10`}
-      >
-        <ul className="overflow-x-hidden max-h-64">
-          {suggestions.map((suggestion) => (
-            <li
-              className={`hover-${theme} cursor-pointer px-2 py-1 rounded-xl mx-1`}
-              key={suggestion.id}
-              id={suggestion.id}
-              onClick={handleSelect}
-            >
-              <h3 className="text-lg underline pointer-events-none text-bold">
-                {suggestion.text_en}
-              </h3>
-              {suggestion.context.map((ctx, index) =>
-                suggestion.context.length === index + 1
-                  ? `${ctx.text_en}.`
-                  : `${ctx.text_en}, `
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
+    <div
+      ref={listNode}
+      className={`absolute transition duration-300 bg-white w-full rounded-xl shadow p-2 z-10 ${
+        !showSuggestions && 'opacity-0 invisible'
+      }`}
+    >
+      <ul className="overflow-x-hidden max-h-64">
+        {suggestions.map((suggestion) => (
+          <li
+            className={`hover-${theme} cursor-pointer px-2 py-1 rounded-xl mx-1`}
+            key={suggestion.id}
+            id={suggestion.id}
+            onClick={handleSelect}
+          >
+            <h3 className="text-lg underline pointer-events-none text-bold">
+              {suggestion.text_en}
+            </h3>
+            {suggestion.context.map((ctx, index) =>
+              suggestion.context.length === index + 1
+                ? `${ctx.text_en}.`
+                : `${ctx.text_en}, `
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
