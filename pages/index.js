36c29955929b8aed5119cc2d2569b528ceac2dbe
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-06-14 13:31:04 +0530

    updated grid layout

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,18 @@ const Home = () => (
       <title>SHNK | Weather</title>
       <link rel="icon" type="image/png" href="/weather-icon.png" />
       <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Weather lookup from SHNK" />
       <meta name="keywords" content="weather, javascript, html, css" />
       <meta name="author" content="shnk.tech" />
     </Head>
     <div className="flex-1 w-full max-w-screen-lg mx-auto">
       <div className="grid gap-4 px-4 md:grid-cols-3">
-        <SearchCard />
-        <WeatherCard />
-        <div className="md:col-start-2 md:row-span-2 md:col-span-2 md:row-start-1">
+        <div className="space-y-4">
+          <SearchCard />
+          <WeatherCard />
+        </div>
+        <div className="md:col-span-2">
           <WeatherChart />
         </div>
         <div className="md:col-span-full">
