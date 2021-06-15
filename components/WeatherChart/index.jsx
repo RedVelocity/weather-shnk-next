b1448ca2d4baf0579cfa994fd8d137d455312f2e
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-06-15 19:41:21 +0530

    added loader

--- components/WeatherChart/index.jsx
+++ components/WeatherChart/index.jsx
@@ -11,8 +11,8 @@ const WeatherChart = () => {
   if (weatherData.daily) data = shapeChartData(weatherData);
 
   return (
-    <div className="flex flex-col min-h-full px-6 py-4 text-gray-200 card justify-evenly bg-dark">
-      {weatherData.daily[0] && (
+    <div className="flex flex-col min-h-full px-6 py-4 space-y-2 text-gray-200 card justify-evenly bg-dark">
+      {weatherData.daily[0] ? (
         <>
           <h1 className="mb-4 text-xl font-semibold tracking-wide capitalize">
             {`${weatherData.daily[0].weather[0].description} on ${dayjs
@@ -20,6 +20,13 @@ const WeatherChart = () => {
               .format('dddd')}`}
           </h1>
           <Bar height={100} data={data} options={options} />
+        </>
+      ) : (
+        <>
+          <span className="w-full h-8 rounded bg-gradient-to-r from-gray-400 to-gray-500" />
+          <span className="w-full h-8 rounded bg-gradient-to-r from-gray-400 to-gray-500" />
+          <span className="w-full h-8 rounded bg-gradient-to-r from-gray-400 to-gray-500" />
+          <span className="w-full h-8 rounded bg-gradient-to-r from-gray-400 to-gray-500" />
         </>
       )}
     </div>
