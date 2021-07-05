3b6cdc6d357b19c13dfaf4014de611339551a20c
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-07-05 18:46:31 +0530

    update skeleton gradient

--- components/Skeleton.jsx
+++ components/Skeleton.jsx
@@ -1,9 +1,8 @@
 const Skeleton = ({ rows = 1 }) =>
   [...Array(rows)].map((_, i) => (
     <div
-      // eslint-disable-next-line react/no-array-index-key
       key={i}
-      className="w-full h-4 rounded md:h-6 bg-gradient-to-r from-gray-300 to-gray-500"
+      className="w-full h-4 rounded md:h-6 bg-gradient-to-r from-cool to-mild"
     />
   ));
 
