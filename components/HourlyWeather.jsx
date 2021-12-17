4bdd6afcd0d85e16fbefc6da327571924339fcb5
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-12-17 17:09:36 +0530

    %s

--- components/HourlyWeather.jsx
+++ components/HourlyWeather.jsx
@@ -1,21 +1,23 @@
 import dayjs from 'dayjs';
 import { useContext } from 'react';
 import { WeatherContext } from '../lib/context/weatherProvider';
-import { getCurrentBreakpoint } from '../lib/utils/getCurrentBreakpoint';
+import { getBreakpointValue } from '../lib/utils/getCurrentBreakpoint';
 import Skeleton from './Skeleton';
 import SummaryCard from './SummaryCard';
+import useWindowSize from '../lib/hooks/useWindowSize';
 
 const HourlyWeather = () => {
   const {
     weatherData: { hourly },
   } = useContext(WeatherContext);
+  const { width } = useWindowSize();
 
   return (
     <>
       {hourly ? (
         <div className="grid min-h-full grid-cols-3 text-gray-200 divide-x divide-y md:grid-cols-5 card bg-dark divide-gray-50/25">
           {hourly
-            .slice(0, getCurrentBreakpoint() === 'sm' ? 6 : 10)
+            .slice(0, width <= getBreakpointValue('md') ? 6 : 10)
             .map((item, i) => (
               <SummaryCard
                 key={i}
