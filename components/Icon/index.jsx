713d6c0a884874fa0c6cc000498b4676b8cdfe3c
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-02-07 15:17:30 +0530

    animate icons

--- components/Icon/index.jsx
+++ components/Icon/index.jsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
-// import { LazyMotion, m, domAnimation } from 'framer-motion';
+import { LazyMotion, m, domAnimation } from 'framer-motion';
 import PropTypes from 'prop-types';
 import { weatherIcons } from './config';
 
 const getIcon = (icon) => `/assets/weather-icons/${weatherIcons[icon]}.svg`;
 
 const Icon = ({ icon, size }) => (
-  // <LazyMotion features={domAnimation} strict>
-  //   <m.div
-  //     initial={{ opacity: 0, width: 0, height: 0 }}
-  //     animate={{ opacity: 1, width: 'auto', height: 'auto' }}
-  //   >
+  <LazyMotion features={domAnimation} strict>
+    <m.div
+      initial={{ opacity: 0, scale: 1.4 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      key={icon}
+    >
       <Image
         src={getIcon(icon)}
         height={size}
@@ -18,8 +20,8 @@ const Icon = ({ icon, size }) => (
         alt="icon"
         priority
       />
-  //   </m.div>
-  // </LazyMotion>
+    </m.div>
+  </LazyMotion>
 );
 
 Icon.propTypes = {
