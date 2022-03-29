7c66d94ec55c344547ededfb63ab939a509f66bd
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-03-29 11:55:30 +0530

    changed footer text color

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 const Footer = () => (
   <footer className="font-semibold text-dark">
     <div className="flex flex-col justify-between max-w-screen-lg gap-2 p-4 m-auto text-sm text-center md:flex-row md:text-lg">
       <a href="https://openweathermap.org/">Powered by OpenWeather</a>
       <span>
         Icons made by{' '}
-        <a href="https://www.freepik.com" title="Freepik" className="text-hot">
+        <a href="https://www.freepik.com" title="Freepik" className="text-cool">
           Freepik
         </a>{' '}
         from{' '}
