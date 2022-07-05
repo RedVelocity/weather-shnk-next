5a5486c1ed1b4c7c9c83b58f2b081828a8aa03d6
Shah Nawaz Khan <blooshaanster@gmail.com>
2022-07-05 14:28:37 +0530

    reduce HourlyForecast for mobiles

--- components/HourlyWeather.jsx
+++ components/HourlyWeather.jsx
@@ -13,26 +13,24 @@ const HourlyWeather = () => {
   return (
     <div className="min-h-full card bg-dark p-4 tracking-wide text-lg">
       <h1 className="text-xl font-semibold text-gray-200">Hourly Forecast</h1>
-      <Grid minColSize="grid-cols-1" maxColSize="sm:grid-cols-2" gap="gap-1">
+      <Grid minColSize="grid-cols-2" maxColSize="sm:grid-cols-2" gap="gap-1">
         {hourly
           .slice(0, width <= getBreakpointValue('md') ? 12 : 12)
           .map((item, i) => (
             <div
-              className="mini-card grid grid-cols-4 gap-2 text-slate-50 items-center"
+              className="mini-card sm:grid flex sm:grid-cols-4 gap-2 text-slate-50 items-center justify-around"
               key={`hourly ${i}`}
             >
-              <span className="font-medium">
+              <span className="font-medium tracking-wider">
                 {i !== 0 ? `${dayjs.unix(item.dt).format('HH:mm')} ` : 'Now'}
               </span>
-              <div className="col-span-2 flex items-center gap-3">
+              <div className="sm:col-span-2 items-center justify-center sm:gap-3 flex">
                 <Icon size={26} icon={item.weather.icon} />
-                <span className="text-center w-full md:text-left">
+                <span className="text-center w-full md:text-left hidden sm:block">
                   {item.weather.description}
                 </span>
               </div>
-              <span className="font-semibold">{`${Math.round(
-                item.temp
-              )}°C`}</span>
+              <span className="">{`${Math.round(item.temp)}°C`}</span>
             </div>
           ))}
       </Grid>
