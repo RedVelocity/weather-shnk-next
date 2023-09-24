eabdb2857177307af5a8e6eb2124a8f0249e3c79
Shaan <blooshaanster@gmail.com>
2023-09-24 17:06:23 +0530

    added footer

--- app/weather/page.jsx
+++ app/weather/page.jsx
@@ -10,6 +10,7 @@ import SearchCard from '@/components/SearchCard';
 import WeatherInfoCardList from '@/components/WeatherInfoCardList';
 import HourlyWeather from '@/components/HourlyWeather';
 import DailyWeather from '@/components/DailyWeather';
+import Footer from '@/components/footer';
 
 const DynamicWeatherMap = dynamic(() => import('../../components/WeatherMap'), {
   loading: () => null,
@@ -38,33 +39,39 @@ const Home = async ({ searchParams }) => {
   const weather = await fetchWeather(location.latitude, location.longitude);
   // console.log(location);
   return (
-    <main className="max-w-screen-xl mx-auto p-2 min-w-[350px]">
+    <>
       <Header hostName={host.hostName} hostUrl={host.hostUrl} />
-      <HydrateAtoms weather={weather} location={location} />
-      <div className="grid gap-3 mx-4 md:grid-cols-3">
-        <section className="space-y-3 flex flex-col">
-          <SearchCard />
-          <div className="flex flex-col sm:flex-row gap-2 flex-1">
-            <WeatherCard />
-            {/* Reposition Component on small devices */}
-            <WeatherInfoCardList className="grid md:hidden" weather={weather} />
-          </div>
-        </section>
-        <section className="md:col-span-2">
-          <HourlyWeather />
-        </section>
-        <section className="md:space-y-3 md:flex md:flex-col">
-          <WeatherInfoCardList className="hidden md:grid" />
-          <DynamicWeatherMap
-            longitude={location.longitude}
-            latitude={location.latitude}
-          />
-        </section>
-        <section className="md:col-span-2">
-          <DailyWeather />
-        </section>
-      </div>
-    </main>
+      <main className="flex-1 min-w-full">
+        <HydrateAtoms weather={weather} location={location} />
+        <div className="grid gap-3 mx-4 md:grid-cols-3">
+          <section className="space-y-3 flex flex-col">
+            <SearchCard />
+            <div className="flex flex-col sm:flex-row gap-2 flex-1">
+              <WeatherCard />
+              {/* Reposition Component on small devices */}
+              <WeatherInfoCardList
+                className="grid md:hidden"
+                weather={weather}
+              />
+            </div>
+          </section>
+          <section className="md:col-span-2">
+            <HourlyWeather />
+          </section>
+          <section className="md:space-y-3 md:flex md:flex-col">
+            <WeatherInfoCardList className="hidden md:grid" />
+            <DynamicWeatherMap
+              longitude={location.longitude}
+              latitude={location.latitude}
+            />
+          </section>
+          <section className="md:col-span-2">
+            <DailyWeather />
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
 
