05108d9874c0e6ca8018f7a6c8f17901a9d0dcb4
Shaan <blooshaanster@gmail.com>
2023-09-26 17:34:45 +0530

    revert maxbox ssr=false

--- components/WeatherMap.jsx
+++ components/WeatherMap.jsx
@@ -1,22 +1,22 @@
-// 'use client';
+'use client';
 
-import Image from 'next/image';
+// import Image from 'next/image';
 import PropTypes from 'prop-types';
-// import useMap from '@/lib/hooks/useMap';
+import useMap from '@/lib/hooks/useMap';
 
-// const mapContainerRef = useMap(longitude, latitude);
-// return <div className="h-80 md:h-full card" ref={mapContainerRef} />;
-const WeatherMap = ({ longitude, latitude }) => (
-  <div className="h-80 md:h-full card relative overflow-hidden">
-    <Image
-      fill
-      src={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s+444(${longitude},${latitude})/${longitude},${latitude},9.5,0,0/400x400?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}`}
-      className="object-cover"
-      quality={100}
-      alt="map"
-    />
-  </div>
-);
+const WeatherMap = ({ longitude, latitude }) => {
+  const mapContainerRef = useMap(longitude, latitude);
+  return <div className="h-80 md:h-full card" ref={mapContainerRef} />;
+  // <div className="h-80 md:h-full card relative overflow-hidden">
+  //   <Image
+  //     fill
+  //     src={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/pin-s+444(${longitude},${latitude})/${longitude},${latitude},9.5,0,0/400x400?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}`}
+  //     className="object-cover"
+  //     quality={100}
+  //     alt="map"
+  //   />
+  // </div>
+};
 
 WeatherMap.propTypes = {
   longitude: PropTypes.number.isRequired,
