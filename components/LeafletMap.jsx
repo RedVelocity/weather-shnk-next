5a27a0bab5972ffcb80ad47af38abd16933486ce
Shaan <blooshaanster@gmail.com>
2024-01-07 20:46:00 +0530

    migrate to leaflet>remove interaction

--- components/LeafletMap.jsx
+++ components/LeafletMap.jsx
@@ -7,7 +7,13 @@ import 'leaflet/dist/leaflet.css';
 // import colors from '../../styles/colors';
 
 const LeafletMap = ({ longitude, latitude }) => (
-  <MapContainer center={[latitude, longitude]} zoom={9} scrollWheelZoom={false}>
+  <MapContainer
+    center={[latitude, longitude]}
+    zoom={10}
+    scrollWheelZoom={false}
+    touchZoom={false}
+    dragging={false}
+  >
     <TileLayer
       url={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}`}
       attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
