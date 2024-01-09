de16cf44911836268852685b2e3e685296b80b27
Shaan <blooshaanster@gmail.com>
2024-01-09 15:48:00 +0530

    Add Mapbox Backend Key

--- app/api/getLocation/route.js
+++ app/api/getLocation/route.js
@@ -20,7 +20,7 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const latitude = searchParams.get('latitude');
   const longitude = searchParams.get('longitude');
-  const API_ENDPOINT = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}&types=place&language=en&limit=1`;
+  const API_ENDPOINT = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_BACKEND}&types=place&language=en&limit=1`;
   const res = await fetch(API_ENDPOINT);
   const data = await res.json();
   return NextResponse.json(data.features[0].place_name);
