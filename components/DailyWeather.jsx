f5fcf1a2f677e97c0741b6cc2a0cab7da2d59a6a
RedVelocity <blooshaanster@gmail.com>
2024-03-26 17:11:06 +0530

    correct alignment in dailyweather

--- components/DailyWeather.jsx
+++ components/DailyWeather.jsx
@@ -16,10 +16,10 @@ const DailyWeather = () => {
       <div className="grid gap-2 mt-4 text-center capitalize sm:grid-cols-2">
         {daily.map((item, i) => (
           <div
-            className="grid grid-cols-2 gap-1 p-4 place-items-center card"
+            className="grid grid-cols-2 gap-1 p-4 place-items-stretch card"
             key={`summary-${i}`}
           >
-            <div className="flex items-center justify-center gap-3">
+            <div className="flex items-center gap-3">
               <Icon icon={item.weather.icon} size={48} />
               <h4 className="leading-4">
                 {i !== 0
@@ -29,7 +29,7 @@ const DailyWeather = () => {
                   : 'Today'}
               </h4>
             </div>
-            <div className="flex flex-col items-center justify-center gap-2">
+            <div className="flex flex-col items-end justify-center gap-2">
               <span className="text-lg">{item.weather.description}</span>
               <h4>{`${Math.round(item.temp.min)}°C • ${Math.round(
                 item.temp.max
