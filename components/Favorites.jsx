ce903a1ac487dd96e014dac7e8e83b3d6aa9173f
Shaan <blooshaanster@gmail.com>
2024-04-07 06:43:12 +0530

    Update Favorites.jsx

--- components/Favorites.jsx
+++ components/Favorites.jsx
@@ -38,7 +38,7 @@ const Favorites = () => (
   </div>
 );
 
-export const FavButton = ({ favKey }) => {
+const FavButton = ({ favKey }) => {
   const { location } = useLocation();
   const [fav, setFav, removeFav] = useLocalStorage({
     key: favKey,
@@ -46,59 +46,60 @@ export const FavButton = ({ favKey }) => {
   });
   const renderFav = fav !== 'unset';
   const [locName, ...locRegion] = renderFav ? fav.name.split(',') : ['', ''];
-  return (
-    <>
-      {renderFav ? (
-        <motion.div
-          variants={variants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          key={`${renderFav}-${favKey}`}
-          className="relative p-4 h-28 card bg-slate-300 sm:h-auto"
+
+  if (renderFav) {
+    return (
+      <motion.div
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        key={`${renderFav}-${favKey}`}
+        className="relative p-4 h-28 card bg-slate-300 sm:h-auto"
+      >
+        <Link
+          href={`weather?q=${encodeURI(fav.name.replaceAll(', ', ','))}`}
+          passHref
+          className="flex flex-col items-center justify-center h-full text-center"
+          scroll={false}
         >
-          <Link
-            href={`weather?q=${encodeURI(fav.name.replaceAll(', ', ','))}`}
-            passHref
-            className="flex flex-col items-center justify-center h-full text-center"
-            scroll={false}
-          >
-            <h3>{locName}</h3>
-            <p>{locRegion.join(', ')}</p>
-          </Link>
-          <button
-            type="button"
-            onClick={removeFav}
-            className="absolute top-0 right-0 z-20 h-6 m-2 aspect-square"
-          >
-            <Image
-              src="/assets/weather-icons/close.png"
-              layout="fill"
-              alt="Close"
-            />
-          </button>
-        </motion.div>
-      ) : (
-        <motion.button
-          variants={variants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          key={`${renderFav}-${favKey}`}
+          <h3>{locName}</h3>
+          <p>{locRegion.join(', ')}</p>
+        </Link>
+        <button
           type="button"
-          className="flex items-center justify-center p-4 h-28 sm:h-auto card"
-          onClick={() => setFav(location)}
+          onClick={removeFav}
+          className="absolute top-0 right-0 z-20 h-6 m-2 aspect-square"
         >
           <Image
-            src="/assets/weather-icons/add.png"
-            height={50}
-            width={50}
-            alt="Add"
+            src="/assets/weather-icons/close.png"
+            layout="fill"
+            alt="Close"
           />
-        </motion.button>
-      )}
-    </>
-  );
+        </button>
+      </motion.div>
+    );
+  } else {
+    return (
+      <motion.button
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        key={`${renderFav}-${favKey}`}
+        type="button"
+        className="flex items-center justify-center p-4 h-28 sm:h-auto card"
+        onClick={() => setFav(location)}
+      >
+        <Image
+          src="/assets/weather-icons/add.png"
+          height={50}
+          width={50}
+          alt="Add"
+        />
+      </motion.button>
+    );
+  }
 };
 
 export default Favorites;
