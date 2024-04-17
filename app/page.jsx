e12e996682c92e13828b143c66fabe2d77a9b777
RedVelocity <blooshaanster@gmail.com>
2024-04-17 19:49:28 +0530

    ^implement^ IP^ geolocation^ v1^

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
+import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 const Home = async () => {
+  const header = headers();
+  const IP = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
+  console.log('IP', IP);
   redirect('/weather?q=Scranton,Pennsylvania,USA');
 };
 
