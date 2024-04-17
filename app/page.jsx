ced2b5d5c8ec5451ba5317a73b8b4e93ff74743b
RedVelocity <blooshaanster@gmail.com>
2024-04-17 20:05:14 +0530

    ^implement^ IP^ geolocation^ v3^

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,15 @@ import { redirect } from 'next/navigation';
 
 const Home = async () => {
   const header = headers();
-  const IP = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
+  const IP = (header.get('x-real-ip') ?? '127.0.0.1').split(',')[0];
   const res = await fetch(
     `http://ip-api.com/json/${IP}?fields=status,country,regionName,city`
   );
   !res.ok && redirect('/weather?q=Scranton,Pennsylvania,USA');
   const location = await res.json();
+  const { country, regionName, city } = location;
   console.log('IP', IP, location);
-  redirect('/weather?q=Scranton,Pennsylvania,USA');
+  redirect(`/weather?q=${city},${regionName},${country}`);
 };
 
 export default Home;
