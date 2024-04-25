c8e12d9c16597a3c3f75a6d2df2add91106ef906
RedVelocity <blooshaanster@gmail.com>
2024-04-26 01:56:07 +0530

    ^refactor^ weather/page.jsx^

--- app/weather/page.jsx
+++ app/weather/page.jsx
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation';
+import { headers } from 'next/headers';
+import Image from 'next/image';
 import { getLocation, getWeather } from '@/app/actions';
-// import HydrateAtoms from '@/components/HydrateAtoms';
 import WeatherCard from '@/components/WeatherCard';
 import SearchCard from '@/components/SearchCard';
 import WeatherInfoCardList from '@/components/WeatherInfoCardList';
@@ -8,26 +9,7 @@ import HourlyWeather from '@/components/HourlyWeather';
 import DailyWeather from '@/components/DailyWeather';
 import WeatherMap from '@/components/WeatherMap';
 import Favorites from '@/components/Favorites';
-import { headers } from 'next/headers';
 import UpdateParams from '@/components/UpdateParams';
-import Image from 'next/image';
-
-// export const generateMetadata = async ({ searchParams }) => {
-//   // read route params
-//   const { q } = searchParams;
-//   const locationName = decodeURIComponent(q).split(',')[0];
-//   return {
-//     title: `Weather | ${locationName}`,
-//     description: `Checkout the weather details for ${locationName}`,
-//     keywords: ['Next.js', 'React', 'JavaScript', 'Weather', locationName],
-//     authors: [{ name: 'RedVelocity', url: 'https://redvelo.site' }],
-//     creator: 'RedVelocity',
-//     publisher: 'RedVelocity',
-//     icons: {
-//       icon: '/weather-icon.png',
-//     },
-//   };
-// };
 
 export const metadata = {
   title: `Weather | redvelo.site`,
@@ -49,44 +31,44 @@ export const metadata = {
   },
 };
 
+const DEFAULT_LOCATION = {
+  name: 'Scranton,Pennsylvania,USA',
+  latitude: 41.411835,
+  longitude: -75.665245,
+  curLat: 0,
+  curLon: 0,
+};
+async function getIPLocation() {
+  const header = headers();
+  const IP = (header.get('x-real-ip') ?? '127.0.0.1').split(',')[0];
+  if (IP === '127.0.0.1') {
+    return DEFAULT_LOCATION;
+  }
+  const res = await fetch(
+    `http://ip-api.com/json/${IP}?fields=status,country,regionName,city,lat,lon`
+  );
+  const data = await res.json();
+  if (!res.ok || data?.status === 'fail') {
+    return DEFAULT_LOCATION;
+  }
+  const { country, regionName, city, lat, lon } = data;
+  return {
+    name: `${city},${regionName},${country}`,
+    latitude: lat,
+    longitude: lon,
+    curLat: 0,
+    curLon: 0,
+  };
+}
+
 const Home = async ({ searchParams }) => {
   const { q } = searchParams;
   const query = decodeURIComponent(q);
-
   let location;
   let weather;
   try {
-    if (query !== 'undefined') location = await getLocation(query);
-    else {
-      const header = headers();
-      const IP = (header.get('x-real-ip') ?? '127.0.0.1').split(',')[0];
-      let res;
-      let data;
-      if (IP !== '127.0.0.1') {
-        res = await fetch(
-          `http://ip-api.com/json/${IP}?fields=status,country,regionName,city,lat,lon`
-        );
-        data = await res.json();
-      }
-      if (IP === '127.0.0.1' || !res.ok || data?.status === 'fail') {
-        location = {
-          name: 'Scranton,Pennsylvania,USA',
-          latitude: 41.411835,
-          longitude: -75.665245,
-          curLat: 0, // Use if you'd like localized results
-          curLon: 0,
-        };
-      } else {
-        const { country, regionName, city, lat, lon } = data;
-        location = {
-          name: `${city},${regionName},${country}`,
-          latitude: lat,
-          longitude: lon,
-          curLat: 0, // Use if you'd like localized results
-          curLon: 0,
-        };
-      }
-    }
+    location =
+      query !== 'undefined' ? await getLocation(query) : await getIPLocation();
     if (!location.latitude) return notFound();
     weather = await getWeather(location.latitude, location.longitude);
   } catch (error) {
@@ -96,9 +78,8 @@ const Home = async ({ searchParams }) => {
 
   return (
     <>
-      {/* <HydrateAtoms weather={weather} location={location} /> */}
       <UpdateParams location={location} />
-      <div className="fixed inset-0 -z-10">
+      <div className="fixed top-0 left-0 right-0 h-[100dvh] -z-10 object-cover">
         <Image
           className="blur-sm"
           src="/assets/bg.png"
