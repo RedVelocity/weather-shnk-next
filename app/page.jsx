71dfc7f3621a6f217335e429680ffeef7974560e
Shaan <blooshaanster@gmail.com>
2024-05-08 15:08:51 +0530

    add trycatch in home

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,24 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
+// import { headers } from "next/headers";
+// import { redirect } from "next/navigation";
 
 const Home = async () => {
-  try {
-    const header = headers();
-    const IP = (header.get("x-real-ip") ?? "127.0.0.1").split(",")[0];
-    IP === "127.0.0.1" && redirect("/weather?q=Scranton,Pennsylvania,USA");
-    const res = await fetch(
-      `http://ip-api.com/json/${IP}?fields=status,country,regionName,city`
-    );
-    const data = await res.json();
-    (!res.ok || data.status === "fail") &&
-      redirect("/weather?q=Scranton,Pennsylvania,USA");
-    const { country, regionName, city } = data;
-    redirect(`/weather?q=${city},${regionName},${country}`);
-  } catch (error) {
-    console.error(error);
-    redirect("/weather?q=Scranton,Pennsylvania,USA");
-  }
+  // try {
+  //   const header = headers();
+  //   const IP = (header.get("x-real-ip") ?? "127.0.0.1").split(",")[0];
+  //   IP === "127.0.0.1" && redirect("/weather?q=Scranton,Pennsylvania,USA");
+  //   const res = await fetch(
+  //     `http://ip-api.com/json/${IP}?fields=status,country,regionName,city`
+  //   );
+  //   const data = await res.json();
+  //   (!res.ok || data.status === "fail") &&
+  //     redirect("/weather?q=Scranton,Pennsylvania,USA");
+  //   const { country, regionName, city } = data;
+  //   redirect(`/weather?q=${city},${regionName},${country}`);
+  // } catch (error) {
+  //   console.error(error);
+  //   redirect("/weather?q=Scranton,Pennsylvania,USA");
+  // }
+  redirect("/weather");
 };
 
 export default Home;
