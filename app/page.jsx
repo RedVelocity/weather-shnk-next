f56a2d9d3e69b57e896a0d8ea271a92f3034024b
RedVelocity <blooshaanster@gmail.com>
2024-05-10 21:16:50 +0530

    ^remove^ catch^ from^ Home^

--- app/page.jsx
+++ app/page.jsx
@@ -29,22 +29,16 @@ export const metadata = {
 };
 
 const Home = async () => {
-  try {
-    const header = headers();
-    const IP = (header.get('x-real-ip') ?? '127.0.0.1').split(',')[0];
-    IP === '127.0.0.1' && redirect('/Scranton,Pennsylvania,USA');
-    const res = await fetch(
-      `http://ip-api.com/json/${IP}?fields=status,country,regionName,city`
-    );
-    const data = await res.json();
-    (!res.ok || data.status === 'fail') &&
-      redirect('/Scranton,Pennsylvania,USA');
-    const { country, regionName, city } = data;
-    redirect(`/${city},${regionName},${country}`);
-  } catch (error) {
-    console.error(error);
-    redirect('/Scranton,Pennsylvania,USA');
-  }
+  const header = headers();
+  const IP = (header.get('x-real-ip') ?? '127.0.0.1').split(',')[0];
+  IP === '127.0.0.1' && redirect('/Scranton,Pennsylvania,USA');
+  const res = await fetch(
+    `http://ip-api.com/json/${IP}?fields=status,country,regionName,city`
+  );
+  const data = await res.json();
+  (!res.ok || data.status === 'fail') && redirect('/Scranton,Pennsylvania,USA');
+  const { country, regionName, city } = data;
+  redirect(`/${city},${regionName},${country}`);
 };
 
 export default Home;
